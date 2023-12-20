fd7463d55ad8a9d7b911d91b8fbb81ad5772c260
e29a <132907635+e29a@users.noreply.github.com>
2023-12-20 11:49:09 +0000

    1.3, - 1.5

--- 1/courseinfo/src/App.jsx
+++ 1/courseinfo/src/App.jsx
@@ -8,7 +8,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <>
-     <p>{props.part.name} {props.part.exercises}</p>
+      <p>{props.part.name} {props.part.exercises}</p>
     </>
   )
 }
@@ -31,27 +31,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    title: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts}/>
+      <Header course={course.title} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
